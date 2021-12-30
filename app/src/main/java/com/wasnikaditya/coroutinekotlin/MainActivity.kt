106476336e93e4e57aa76e777eb67f67b5bdc62b
Aditya Wasnik <wasnikaditya56@gmail.com>
2021-12-30 10:35:09 +0530

    Initial commit

---
+++ app/src/main/java/com/wasnikaditya/coroutinekotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wasnikaditya.coroutinekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
